99b9e329bdae60c4c63bb876a7db4ebad72dbbb1
Moredain <avatarshef@yandex.ru>
2016-05-13 17:39:30 +0300

    123

---
+++ markup.py
@@ -0,0 +1,20 @@
+from markdown import markdown
+import pdfkit
+
+input_filename = 'bootstrap_mod/example.html'
+output_filename = 'README.pdf'
+
+with open(input_filename, 'r') as f:
+    html_text = markdown(f.read(), output_format='html4')
+
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.25in',
+    'margin-right': '0.25in',
+    'margin-bottom': '0.25in',
+    'margin-left': '0.25in',
+    'encoding': "UTF-8",
+    'no-outline': None
+}
+
+pdfkit.from_string(html_text, output_filename, options=options)
