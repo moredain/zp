d66c79ba2ebbeb055c1d80e86146181185888f4b
Moredain <avatarshef@yandex.ru>
2016-05-13 19:36:00 +0300

    123

--- markup.py
+++ markup.py
@@ -1,7 +1,10 @@
+#coding: utf8
+
 from markdown import markdown
 import pdfkit
 
 input_filename = 'bootstrap_mod/example.html'
+
 output_filename = 'README.pdf'
 
 with open(input_filename, 'r') as f:
@@ -17,4 +20,9 @@ options = {
     'no-outline': None
 }
 
-pdfkit.from_string(html_text, output_filename, options=options)
+
+css = ['bootstrap_mod/example/bootstrap.min.css', 'bootstrap_mod/example/application.css',
+       'bootstrap_mod/example/discovery.449c10a436c997c3eee5afd5021da3a2.css','bootstrap_mod/example/docs.min.css',
+       'bootstrap_mod/example/lounge.f668c82e65f78693a6a62a5c7d3e54b8.css',]
+
+pdfkit.from_string(html_text, output_filename, options=options, css=css)
