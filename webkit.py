2d865139ce95b2aa635dfadf0c629e33300c59b4
Moredain <avatarshef@yandex.ru>
2016-05-19 19:18:43 +0300

    123

--- webkit.py
+++ webkit.py
@@ -12,15 +12,31 @@ app1 = QApplication(sys.argv)
 
 web = QWebView()
 
-#local_url = QUrl.fromLocalFile('bootstrap_mod/example.html')
+style_url1 = 'file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example/bootstrap.min.css'
+web.settings().setUserStyleSheetUrl(QUrl(style_url1))
+style_url2 = 'file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example/application.css'
+web.settings().setUserStyleSheetUrl(QUrl(style_url2))
+style_url3 = 'file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example/discovery.449c10a436c997c3eee5afd5021da3a2.css'
+web.settings().setUserStyleSheetUrl(QUrl(style_url3))
+style_url4 = 'file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example/docs.min.css'
+web.settings().setUserStyleSheetUrl(QUrl(style_url4))
+style_url5 = 'file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example/lounge.f668c82e65f78693a6a62a5c7d3e54b8.css'
+web.settings().setUserStyleSheetUrl(QUrl(style_url5))
+
+
+
+
 web.load(QUrl('file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example.html'))
+
 
 #web.setStyleSheet("qrc:/bootstrap_mod/example/bootstrap.min.css")
 #view.settings().setUserStyleSheetUrl(QUrl.fromLocalFile("style.css"))
 
-style_url = 'file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/example/bootstrap.min.css'
+
+
 #web.settings().
 # web.show() можно показать страницу
+
 printer = QPrinter()
 printer.setPageSize(QPrinter.A4)
 printer.setOutputFormat(QPrinter.PdfFormat)
