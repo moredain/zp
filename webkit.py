2c87212e60108196274cd434e71bbd156a20f138
Moredain <avatarshef@yandex.ru>
2016-05-31 20:11:25 +0300

    123

--- webkit.py
+++ webkit.py
@@ -1,19 +1,31 @@
 import sys
 
-from PyQt5.QtCore import QUrl, QObject,pyqtSignal
+from PyQt5.QtCore import QUrl
 
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtWidgets import QApplication
 
-from PyQt5.QtPrintSupport import QPrintDialog, QPrinter, QPrintPreviewDialog, QPrinterInfo
-from PyQt5.QtWebKitWidgets import QWebPage, QWebView
-from PyQt5.QtWebKit import QWebSettings
+from PyQt5.QtPrintSupport import QPrinter
+from PyQt5.QtWebKitWidgets import QWebView
 
 app1 = QApplication(sys.argv)
 
 web = QWebView()
 
+#web.load(index.txt)
+#web.load(QUrl('file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/index.html'))
 
-web.load(QUrl('file:///home/umbrella/PycharmProjects/zp/bootstrap_mod/index.html'))
+#pyDir = os.path.abspath(os.path.dirname(__file__))
+#baseUrl = QUrl.fromLocalFile(os.path.join(pyDir, "static/"))
+#html = """
+#    <html><body>
+#    <div>Hello World!</div>
+#    <img src="test.png"/>
+#    </body></html>
+#    """
+#view.setHtml(html, baseUrl)
+
+
+web.load(QUrl ("file:///"+"bootstrap_mod/index.html"))
 
 
 printer = QPrinter()
