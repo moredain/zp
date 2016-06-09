86d6c83dd73ddf3e0826741f58a6b54d07d83658
Moredain <avatarshef@yandex.ru>
2016-06-09 18:38:25 +0300

    123

--- html_generator.py
+++ html_generator.py
@@ -9,22 +9,23 @@ import os
 def todohtml(shtatx):
     temphtml = open("bootstrap_mod/index.html", "r")
     temphtmlstr = temphtml.read()
+    lenstr = len(temphtmlstr)
+    print(lenstr)
     temphtml.close()
-    print(type(temphtmlstr))
-#    print(temphtmlstr)
-    print(shtatx[2])
-    for i in shtatx:
-#        temphtmllist = temphtmlstr.replace("###",'hren1' )
-#        print(temphtmllist)
-        i = 0
-        replx =shtatx[i]
-        repl = replx+str(i)
-        print(repl)
-        temphtmllist = temphtmlstr.replace("###", repl)
-        print(temphtmllist)
-        i= i+1
+    j = 0
+    print(type(j))
+    replx = shtatx[int(j)]
+    print("replx = " + replx)
+    str ="###"
+
+    temphtmllist = temphtmlstr.find(str, 0,lenstr)
+    print(temphtmllist)
+
+
 
 shtat = ['hren1','hren2','hren3','hren4','hren5','hren6','hren7','hren8','hren9','hren10','hren11','hren12','hren13',
          'hren14','hren15','hren16','hren17',]
+shtatcopy =list.copy(shtat)
+print(shtatcopy)
 
 todohtml(shtat)
