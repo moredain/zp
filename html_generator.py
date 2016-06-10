498002567cb970462b0898e92f304625de7909bc
Moredain <avatarshef@yandex.ru>
2016-06-10 18:45:45 +0300

    123

--- html_generator.py
+++ html_generator.py
@@ -9,23 +9,28 @@ import os
 def todohtml(shtatx):
     temphtml = open("bootstrap_mod/index.html", "r")
     temphtmlstr = temphtml.read()
-    lenstr = len(temphtmlstr)
-    print(lenstr)
-    temphtml.close()
-    j = 0
-    print(type(j))
-    replx = shtatx[int(j)]
-    print("replx = " + replx)
-    str ="###"
 
-    temphtmllist = temphtmlstr.find(str, 0,lenstr)
-    print(temphtmllist)
+    temphtml.close()
+    search = temphtmlstr.find("###")
+    if search == -1:
+        print("Error. String of replace don't found")
+    else:
+        mylistlen = len(shtatx)
+        print(shtatx)
+        spli = temphtmlstr.split("###")
+        i = 0
+
+        while i < mylistlen:
+            print("i = " + str(i))
+            spli.insert(1 + i * 2, shtatx[i])
+            print(spli)
+            i = i + 1
+
 
 
 
 shtat = ['hren1','hren2','hren3','hren4','hren5','hren6','hren7','hren8','hren9','hren10','hren11','hren12','hren13',
          'hren14','hren15','hren16','hren17',]
-shtatcopy =list.copy(shtat)
-print(shtatcopy)
 
-todohtml(shtat)
+
+print(todohtml(shtat))
